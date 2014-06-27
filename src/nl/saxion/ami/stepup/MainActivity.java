1149bfc9ea75a67fc23798660ce63479b6d3c19f
wilcobonestroo <w.j.bonestroo@saxion.nl>
2014-06-27 08:07:18 +0200

    Added norm method

--- src/nl/saxion/ami/stepup/MainActivity.java
+++ src/nl/saxion/ami/stepup/MainActivity.java
@@ -7,6 +7,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.util.FloatMath;
 import android.util.Log;
 import android.view.Menu;
 
@@ -52,6 +53,21 @@ public class MainActivity extends Activity implements SensorEventListener {
 	protected void onPause() {
 		super.onPause();
 		mSensorManager.unregisterListener(this);
+	}
+	
+	/**
+	 * Returns the length of a 3d vector.
+	 * @param event
+	 * @return
+	 */
+	public static float norm(SensorEvent event) {
+		assert event.values.length == 3;
+		
+		float result = event.values[0] * event.values[0] +
+				event.values[1] * event.values[1] +
+				event.values[2] * event.values[2];
+		
+		return FloatMath.sqrt(result);
 	}
 
 }
